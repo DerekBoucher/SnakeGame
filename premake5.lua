8c5d445e111c537180c6ff7b9879be97d7b85d37
Derek Boucher <derek.boucher@me.com>
2019-04-18 16:44:17 -0400

    Added comments

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,16 @@
 -- Premake5 BuildScript for snakeGame
+-- Author: Derek Boucher
+-- In order to build, simply open command line in project folder and execute the following:
+-- Xcode (MAC OS):
+-- >premake5 xcode4
+-- Visual Studio 2013 (Windows):
+-- >premake5 vs2013
+-- Visual Studio 2015 (Windows):
+-- >premake5 vs2015
+-- Visual Studio 2017 (Windows):
+-- >premake5 vs2017
+
+-- Then simply open project in IDE and build.
 
 workspace "snakeGame"
 	configurations { "Debug", "Release", "Dist" }
